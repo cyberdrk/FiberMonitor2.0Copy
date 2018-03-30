0e538615cbb6d5f8d027a27d94e5f81a866b7198
Rajneesh Katkam <rajneesh.katkam@gmail.com>
2018-03-30 16:22:00 +0530

    card notifications on recycler view implemented

--- app/src/main/java/com/raj/fibermonitor20copy/MainActivity.java
+++ app/src/main/java/com/raj/fibermonitor20copy/MainActivity.java
@@ -6,6 +6,7 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.View;
@@ -22,6 +23,9 @@ import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
+    RecyclerViewAdapter recyclerViewAdapter;
+
+
     private final Handler mHandler = new Handler();
     private Runnable mTimer1;
     CustomView mumbaiDelhi,delhiKolkata,kolkataSecunderabad,secunderabadChennai,chennaiMumbai;
@@ -36,6 +40,9 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         mp = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
+
+        recyclerView=findViewById(R.id.recyclerView);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext()));
 
         ///Intialization
         mumbaiDelhi=findViewById(R.id.mumbaiDelhi);
@@ -109,6 +116,9 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                     }
+
+                recyclerViewAdapter = new RecyclerViewAdapter(getApplicationContext(), cardNotification);
+                recyclerView.setAdapter(recyclerViewAdapter);
                 Log.d("StatusListSS", String.valueOf(cardNotification.size()));
             }
 
@@ -359,6 +369,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
 
 
 
